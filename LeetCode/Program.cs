ef5101cc76c3e594a94f12f404f2838b4694d254
DenisMarasan <mitreya@yandex.ru>
2023-06-23 14:55:39 +0300

    Добавьте файлы проекта.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        SubstractProductAndSum(234);
+
+        public int SubtractProductAndSum(int n)
+        {
+            string nString = Convert.ToString(n);
+            char[] nChar = nString.ToCharArray();
+            int prod = 0, sum = 0;
+
+            for (int i = 0; i < nChar.Length; i++)
+            {
+                prod += nChar[i];
+                sum += nChar[i];
+            }
+
+            return prod - sum;
+        }
+
+    }
+
+}
